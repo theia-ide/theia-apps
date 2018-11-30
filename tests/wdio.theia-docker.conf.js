2fce4ff0eaf9ce7e6a89098cf5aaa027937278cc
Marc Dumais <marc.dumais@ericsson.com>
2018-11-30 09:37:57 -0500

    [ci] add minimal tests for docker images
    closes #83
    
    This commit add a minimal test suite that validates that the images,
    that we build and publish, include a minimally working Theia application.
    
    Until now we relied on the success of an image's build during CI, to
    publish it on dockerhub. That's not good, because Theia applications
    can sometimes build but not behave correctly or even start.
    
    The UI test suite and related configurations were borrowed from the
    main theia repo and slightly adapted. ATM we use only the most basic
    test suite, "main-page", that should be applicable to all Theia
    applications we run CI on.
    
    This will be enhanced in the future to to add image-specific test
    files. For example, see wdio.theia-docker.conf.js, that adds an
    extra test file to be run, for the theia-docker image. ATM this second
    suite is bogus and just for demonstration purposes.
    
    Signed-off-by: Marc Dumais <marc.dumais@ericsson.com>

---
+++ tests/wdio.theia-docker.conf.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2018 Ericsson and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+
+// tests for the "theia-docker" application
+const wdio = require('./wdio.base.conf.js');
+
+exports.config = wdio.makeConfig(true);
+
+// add here the files containing tests to be run for this
+// application
+exports.config.specs.push('./theia-docker/**/*spec.ts');
