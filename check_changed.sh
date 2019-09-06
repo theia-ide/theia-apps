891e66f856c29c9459fd7f4459d3428c16e779c6
Dinne Bosman <dwjbosman@yahoo.com>
2019-09-06 23:26:17 +0200

    Travis should only build/test images that have changes
    Signed-off-by: Dinne Bosman <dwjbosman@yahoo.com>

---
+++ check_changed.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+# This script is called by Travis during the install step.
+# It returns 1 if no files where changed. In that case
+# no further building/test is required for this image
+
+IMAGE_NAME=$1
+
+echo "Travis event type: $TRAVIS_EVENT_TYPE"
+
+if [ "$TRAVIS_EVENT_TYPE" == "api" ]
+then
+    # trigger via travis dashboard
+    # test all
+    echo "Trigger all tests"
+    exit 0
+fi
+
+cd "$IMAGE_NAME-docker"
+
+CHANGED_FILES=$(git diff --name-status HEAD~1...HEAD .)
+if [ -z "$CHANGED_FILES" ]
+then
+    # nothing changed, skip building
+    echo "No changes in $IMAGE_NAME, terminate"
+   
+    # this indicates to the parent script that the build can be terminated
+    exit 137
+fi
+
+echo "There were changes in $IMAGE_NAME changes: $CHANGED_FILES"
+exit 0
