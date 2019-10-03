755f3dd81c8ca5e5fac9b76122af3f59c378abf5
Dinne Bosman <dwjbosman@yahoo.com>
2019-10-03 09:01:21 +0200

    Integrate VS code -  cortex-debug example

--- theia-rust-embedded-docker/run.sh
+++ theia-rust-embedded-docker/run.sh
@@ -3,6 +3,9 @@ CURDIR=$(pwd)
 echo "container start path: $CONTAINER_START_PATH"
 echo "theia root dir: $CURDIR"
 
+# contains cortex-debug plugin
+export THEIA_DEFAULT_PLUGINS=local-dir:$THEIA_RUST_APP_PATH/plugins
+
 cd $THEIA_RUST_APP_PATH
 yarn theia start $CURDIR --hostname=0.0.0.0 --log-level=debug --verbose
 
