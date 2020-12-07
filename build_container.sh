c3565c576ef0851b08b26205efdba827d6a63729
Duc Nguyen <duc.a.nguyen@ericsson.com>
2020-12-07 15:46:11 -0500

    ci: stabilize 'github actions' builds
    The following commit updates the `github actions` workflow in order to
    stabilize the builds, and align with the upstream `theia` repository.
    The updates include:
    
    + Updating the `runs_on` from `ubuntu-latest` to `ubuntu-18.04`,
      similarly to `theia` upstream.
    + Fixing an issue which incorrectly passes `GITHUB_TOKEN` when calling
      `build_container` script.
    + Updating the documentation to refer to `GitHub Actions` instead of
      `travis`.
    
    Signed-off-by: Duc Nguyen <duc.a.nguyen@ericsson.com>

--- build_container.sh
+++ build_container.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 set -e
 
-# this script is called by Travis to build the Docker image
+# this script is called by GH workflows to build the Docker image
 NPM_TAG=$1
 IMAGE_NAME=$2
 NODE_VERSION=$3
+GH_TOKEN=$4
 
 # Theia standard port is listening in port 3000 (the common nodejs port), and it is exposed when running the docker
 # container (-p 0.0.0.0:4000:3000). But some new applications may need to change it (e.g. theia-https-docker).
-# This 4th parameter enables to expose a custom port instead of the common 3000 port. It is set to 3000 as a default
+# This 5th parameter enables to expose a custom port instead of the common 3000 port. It is set to 3000 as a default
 # value if not included, for backward compatibility.
-PORT=${4:-3000}
+PORT=${5:-3000}
+shift
 shift
 shift
 shift
 
-# We know that there are at least 3 parameters. If we shift the 4th parameter and it is not set, shift will fail thus
+# We know that there are at least 4 parameters. If we shift the 5th parameter and it is not set, shift will fail thus
 # failing the script (because of set -e)
 [ $# -gt 0 ] && shift
 
